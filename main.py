bbec68ce14de74adb98d119a832b9823d6e963d2
ProjetoIntegrador <projint200@gmail.com>
2023-03-28 16:12:42 -0300

    Primeiro commit

---
+++ main.py
@@ -0,0 +1,46 @@
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+import streamlit as st
+from textwrap import wrap
+
+
+with open('style.css') as f:
+    st.markdown(f'<style>{f.read()}</style>', unsafe_allow_html=True)
+
+
+#função dataframe
+
+def mostrar_linhas(dataframe):
+    qtd_linhas = st.sidebar.slider('Selecione a quantidade de linhas para visualização',
+                                   min_value=1, max_value=20, step=1)
+    st.write(dataframe.head(qtd_linhas))
+
+#função gráfico
+
+def plot_vendas(dataframe, categoria):
+    vendas_plot = dataframe.query('Categoria == @categoria')
+    fig, ax = plt.subplots(figsize=(14,6))
+    ax = sns.barplot(x= 'Produto', y = 'Quantidade', data =vendas_plot)
+    ax.set_title(f'Quantidade de vendas de  {categoria}', fontsize=18)
+    ax.set_xlabel('Produto', fontsize=14)
+    plt.ylim(0, 6)
+    ax.tick_params(rotation=25, axis='x')
+    ax.set_ylabel('Quantidade', fontsize=14)
+
+    return fig
+
+vendas = pd.read_excel('bd_vendas.xlsx') #, on_bad_lines='skip', encoding="latin")
+
+st.title('Quantidades de Produtos vendidos por Categoria\n')
+st.write('Período de jun/2017 a ago/2018')
+
+
+
+# filtro para o gráfico
+st.sidebar.markdown('## Filtro para o gráfico')
+
+categoria_grafico = st.sidebar.selectbox('Selecione a Categoria', options = vendas['Categoria'].unique())
+figura = plot_vendas(vendas, categoria_grafico)
+st.pyplot(figura)
+
